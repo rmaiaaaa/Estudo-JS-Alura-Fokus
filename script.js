71466d1da5e3d5e1cd5b5ee533e6184e65ff160c
Rafael Maia de Souza <rmaiadesouza@gmail.com>
2024-02-20 20:43:24 -0300

    Inclusão de modificação de imagens e mensagens no contexto.

--- script.js
+++ script.js
@@ -1,20 +1,58 @@
+const FOCO = 'foco';
+const DESCANSO_CURTO = 'descanso-curto';
+const DESCANSO_LONGO = 'descanso-longo';
+/*----------------------------------------------- */
 const html = document.querySelector('html');
 const botaoFoco = document.querySelector('.app__card-button--foco');
 const botaoDescansoCurto = document.querySelector('.app__card-button--curto');
 const botaoDescansoLongo = document.querySelector('.app__card-button--longo');
-const banner = document.querySelector('.app__image')
+const banner = document.querySelector('.app__image');
+const texto = document.querySelector('.app__title');
 
 botaoFoco.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'foco');
-    banner.setAttribute('src', '/imagens/foco.png')
+    alterarContexto(FOCO);
 });
 
-botaoDescansoCurto.addEventListener('click', ()=> {
-    html.setAttribute('data-contexto', 'descanso-curto');
-    banner.setAttribute('src', '/imagens/descanso-curto.png')
+botaoDescansoCurto.addEventListener('click', () => {
+    alterarContexto(DESCANSO_CURTO);
 });
 
 botaoDescansoLongo.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'descanso-longo');
-    banner.setAttribute('src', '/imagens/descanso-longo.png')
+    alterarContexto(DESCANSO_LONGO);
 });
+
+function alterarContexto(contexto) {
+    html.setAttribute('data-contexto', contexto);
+    banner.setAttribute('src', `/imagens/${contexto}.png`);
+    modificarPorContexto(contexto);
+
+}
+
+function modificarPorContexto(contexto) {
+
+    switch (contexto) {
+        case FOCO:
+            texto.innerHTML = `
+            Otimize sua produtividade,<br>
+                <strong class="app__title-strong">mergulhe no que importa.</strong>
+            `
+            break;
+
+        case DESCANSO_CURTO:
+            texto.innerHTML = `
+            Que tal dar uma respirada?<br>
+                <strong class="app__title-strong">Faça uma pausa curta!</strong>
+            `
+            break;
+        case DESCANSO_LONGO:
+            texto.innerHTML = `
+            Hora de voltar à superfície.<br>
+                <strong class="app__title-strong">Faça uma pausa longa.</strong>
+            `
+            break;
+
+        default:
+            break;
+    }
+
+}
