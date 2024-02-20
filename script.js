6580869634e38e54a91bb66def07ceb7ba58fd4e
Rafael Maia de Souza <rmaiadesouza@gmail.com>
2024-02-20 19:47:15 -0300

    Inclusão de funcionalidade para modificação de imagem

--- script.js
+++ script.js
@@ -2,15 +2,19 @@ const html = document.querySelector('html');
 const botaoFoco = document.querySelector('.app__card-button--foco');
 const botaoDescansoCurto = document.querySelector('.app__card-button--curto');
 const botaoDescansoLongo = document.querySelector('.app__card-button--longo');
+const banner = document.querySelector('.app__image')
 
 botaoFoco.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'foco');
+    banner.setAttribute('src', '/imagens/foco.png')
 });
 
 botaoDescansoCurto.addEventListener('click', ()=> {
     html.setAttribute('data-contexto', 'descanso-curto');
+    banner.setAttribute('src', '/imagens/descanso-curto.png')
 });
 
 botaoDescansoLongo.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'descanso-longo');
+    banner.setAttribute('src', '/imagens/descanso-longo.png')
 });
